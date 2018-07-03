c00b1f29f677ae8d376302f0186e13f77902204e
Mehmet <mdakin@gmail.com>
2018-07-03 15:25:52 +0200

    Added hello world.

---
+++ hello/hello.rs
@@ -0,0 +1,12 @@
+// main function
+fn main() {
+  // Print some stuff to console
+  // println! is a macro, what exactly is a macro? like C macros?
+  println!("Hello"); 
+  println!("World");
+}
+// To run,"$ rustc hello.rs" "$ ./hello"
+// By default creates 5.5MB hello executeable.
+// There is probably a way to create a slimmed down version
+
+// Initial feelings: It seems consise, tendency to use sigils.
